015aaca49c04aff13a7557fb58a3be38ab23ccc4
bodhibrdr <julie.gumerman@gmail.com>
2020-01-20 09:03:24 -0700

    trying to get bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -25,11 +25,30 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
+    #compare pairs side-by-side
+    #if righthand card is smaller than left, swap
+    #if a swap has taken place, do the whole process again (boolean to record it)
+
+    for i in range(1, len(arr)):
+        temporary = arr[i]
+        minimum_index = arr[i]
+        if arr[i + 1] <= len(arr):     
+            if temporary > int(arr[i + 1]):
+                minimum_index = arr[i + 1]
+                temporary, minimum_index = minimum_index, temporary
+
+
+
 
     return arr
 
 
 # STRETCH: implement the Count Sort function below
 def count_sort( arr, maximum=-1 ):
-
+    for i in range(len(arr)):
+        minimum_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[minimum_index] > arr[j]:
+                minimum_index = j
+        arr[i], arr[minimum_index] = arr[minimum_index], arr[i]
     return arr
