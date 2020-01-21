b06373c739f36ca97245cdb61392fa6846b7cd1b
bodhibrdr <julie.gumerman@gmail.com>
2020-01-21 13:51:10 -0700

    maybe making it work

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,3 +1,7 @@
+#divide and conquer!!!!!
+
+
+
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
@@ -10,6 +14,26 @@ def merge( arrA, arrB ):
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+
+    for i in arr:
+        if i % 2 == 0:
+            temp = arr[i]
+            temp_two = arr[i + 1]
+            new_arr = []
+            new_arr.append(temp)
+            new_arr.append(temp_two)
+            new_arr.sort()
+            arr.append(new_arr)
+            arr.remove(temp)
+            arr.remove(temp_two)
+            
+ 
+
+    
+    #split array into a bunch of tiny arrays with two apiece
+    #put those two-number arrays in order
+    #merge into four; sort
+    #merge; sort one last time
 
     return arr
 
